4710f7f873c18246fdc28e475ac5b102be19bdc6
Lysandra <81883243+LysandraBW@users.noreply.github.com>
2024-04-27 19:29:23 -0400

    Add files via upload

---
+++ WorkedOn/Generated-20240427191628.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Saturday-April-27-2024   
+--------------------------------------------------------
+@C:\Users\lbeln\Downloads\WORKEDON.sql
+--@C:\Users\lbeln\Downloads\WORKEDON_DATA_TABLE.csv
+@C:\Users\lbeln\Downloads\SYS_C00425259.sql
+@C:\Users\lbeln\Downloads\WORKEDON_CONSTRAINT.sql
+@C:\Users\lbeln\Downloads\WORKEDON_REFCONSTRAINT.sql
---
+++ WorkedOn/WORKEDON_REFCONSTRAINT.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  Ref Constraints for Table WORKEDON
+--------------------------------------------------------
+
+  ALTER TABLE "LBELNAVISWALTERS"."WORKEDON" ADD FOREIGN KEY ("NCONST")
+	  REFERENCES "VOIGTC"."CREWMEMBER" ("NCONST") ENABLE;
+  ALTER TABLE "LBELNAVISWALTERS"."WORKEDON" ADD FOREIGN KEY ("TCONST")
+	  REFERENCES "VOIGTC"."MEDIA" ("TCONST") ENABLE;
